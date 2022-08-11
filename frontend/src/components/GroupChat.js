c17a89354122d6aa9073ca968177e3226f5a87b6
NicholasK <nicholask1@outlook.com>
2022-08-11 17:58:04 -0400

    Testing websockets

--- frontend/src/components/GroupChat.js
+++ frontend/src/components/GroupChat.js
@@ -28,56 +28,23 @@ const GroupChat = () => {
                 // console.log(JSON.parse(evt.data)) 
                 let data = JSON.parse(evt.data)
                 
-                let messageBody = document.querySelector('.message-list');
-                if(messageBody) {
-                    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-                }
+                
+                
                 if(data === []) {
                     setMessages(null);
                 }
                 else {
                     setMessages(data);
                 }
-               
             }
         }
         fetchGroupMessages();
     })
-  
-    // useEffect(() => {
-    //     async function fetchGroupMessages() {
-    //         let res = await Comet.getGroupMessages(guid);
-    //         let messageBody = document.querySelector('.message-list');
-    //         if(messageBody) {
-    //             messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-    //         }
-
-    //         if(res.data.data === []) {
-    //             setMessages(null);
-    //         }
-    //         else {
-    //             setMessages(res.data.data);
-    //         }
-    //         setTrigger(0)
-            
-    //     }
-    //     fetchGroupMessages();
-    // },[trigger])
-
-
-    // setInterval(function() {
-    //     if(trigger === 0) {
-    //     setTrigger(1)
-    //     }
-    //     else {
-    //         setTrigger(0)
-    //     }
-    // },3000)
-
 
     const sendMessage = async () => {
         await Comet.sendMessage(guid, formData.text, uid);
         setTrigger(1);
+        
         
         
     } 
@@ -88,19 +55,20 @@ const GroupChat = () => {
             ...formData,
             [name]: value
         }))
+        let messageBody = document.querySelector('.message-list');
+        if(messageBody) {
+        messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+        }
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         sendMessage()
         setFormData(INIT_STATE);
-
-        // ws.send(
-        //     {
-        //         guid:guid,
-        //         text: formData.text,
-        //         uid: uid
-        //     })
+        let messageBody = document.querySelector('.message-list');
+        if(messageBody) {
+            messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+        }
     }
 
 
