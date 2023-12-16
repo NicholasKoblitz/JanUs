f28c3156fc51ac648a8e7910f8a955d671bd3ef3
Nick <nick@Sabrinas-MacBook-Pro-4.local>
2023-12-16 15:28:57 -0500

    Trying to handle bug

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const JANUS_URL = process.env.REACT_APP_BASE_URL;
+const JANUS_URL = "https://janus-r5sd.onrender.com/";
 const COMET_URL = "https://215851690468e749.api-us.cometchat.io/v3/";
 const API_KEY = '7a206718ea2245a90d00d043f313ab5192048e02'
 
