2cd93d2db860c4c385120d6a01bf2fbf5e905c32
NicholasKoblitz <nicholask1@outlook.com>
2024-12-20 11:13:17 -0500

    Built Project

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const JANUS_URL = "https://pmbpgyeoydjkhwqgxbay.supabase.co";
+const JANUS_URL = "https://pmbpgyeoydjkhwqgxbay.supabase.co/";
 const COMET_URL = "https://215851690468e749.api-us.cometchat.io/v3/";
 const API_KEY = '7a206718ea2245a90d00d043f313ab5192048e02'
 
